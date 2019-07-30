ab8a169a2fd1326f6cfa93d5b551136aba5be42f
“Hindreen” <hndfaw@gmail.com>
2019-07-30 10:35:45 -0600

    Updates

---
+++ precache-manifest.f5fec0ac9b4e3e741ffdd76fa7afe32a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d1e8c33e46bae6299847fa80b90c2d",
+    "url": "/project-t/index.html"
+  },
+  {
+    "revision": "349334a4645dbc5720c3",
+    "url": "/project-t/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3daa5f316579133d0a82",
+    "url": "/project-t/static/js/2.9fe2f782.chunk.js"
+  },
+  {
+    "revision": "349334a4645dbc5720c3",
+    "url": "/project-t/static/js/main.c78cd4da.chunk.js"
+  },
+  {
+    "revision": "39d9c3ed9cadb61ad177",
+    "url": "/project-t/static/js/runtime~main.fec0b221.js"
+  }
+]);
